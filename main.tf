ede6cd48f0b3445143dd6431ed566e2392455bad
Jamie Taffurelli <jltaffurelli@outlook.com>
2023-01-13 13:55:08 +0000

    added auto registration value

--- main.tf
+++ main.tf
@@ -168,10 +168,11 @@ resource "azurerm_monitor_diagnostic_setting" "virtual_network_diagnostics" {
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "dns_vnet_link" {
-  provider              = azurerm.dns
   for_each              = { for k in var.private_dns_zones : k.name => k if k != null }
   name                  = var.virtual_network_name
   resource_group_name   = each.value["resource_group_name"]
   private_dns_zone_name = each.value["name"]
+  registration_enabled  = each.value["registration_enabled"]
   virtual_network_id    = azurerm_virtual_network.network.id
+  tags                  = var.tags
 }
